ab70cfe24ff18b44f9ec0d1ea6abe802ef074659
kiri <krclemmensen@gmail.com>
2021-09-02 14:39:34 -0400

    added bash script

---
+++ build.sh
@@ -0,0 +1,7 @@
+cat templates/top.html content/index.html templates/bottom.html > docs/index.html
+
+cat templates/top.html content/projects.html templates/bottom.html > docs/projects.html
+
+cat templates/top.html content/about.html templates/bottom.html > docs/about.html
+
+cat templates/top.html content/blog.html templates/bottom.html > docs/blog.html
